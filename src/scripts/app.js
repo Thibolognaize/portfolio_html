a55ed0d1c1505b42536d621da9f31e1826e38e52
Thibolognaize <thibault.feat@gmail.com>
2024-11-14 01:27:45 +0100

    Debugage et ajout du responsive

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+
+})
---
+++ src/scripts/scrollToSection.js
@@ -0,0 +1,16 @@
+
+// function scrollToCompetences(event) {
+//     event.preventDefault(); //Empêche le comportement par défaut du lien
+//     console.log("Fonction scrollToCompetences appelée");
+//     document.querySelector("#competences").scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// }
+
+// function scrollToContact(event) {
+//     event.preventDefault(); //Empêche le comportement par défaut du lien
+//     console.log("Fonction scrollToContact appelée");
+//     document.querySelector("#competences").scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// }
