1f662e6e9ac944cd4dc3349f63f07d77427cac57
Thibolognaize <thibault.feat@gmail.com>
2024-11-16 13:10:57 +0100

    test

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-	<!-- Footer -->
-	<footer class=" w-full bg-slate-950 bg-gradient-to-r from-cyan-950 shadow-xl shadow-slate-950/50 p-6 text-center">
-		<p>&copy; 2024 Thibault Feat. Tous droits réservés.</p>
-	</footer>
-
-	<script src="./src/scripts/app.js"></script>
-</body>
-
-</html>
