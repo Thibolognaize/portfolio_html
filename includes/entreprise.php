dd2063ebc85df19ba1ad3f1283bed53070b4a49d
Thibolognaize <thibault.feat@gmail.com>
2025-06-08 23:11:36 +0200

    mise a jour entrerprise

--- includes/entreprise.php
+++ includes/entreprise.php
@@ -1,13 +1,13 @@
 <section id="Entreprise" class="container mx-auto p-6 mt-10">
-    <h2 class="text-3xl font-bold mb-4">Mon Entreprise : Augefi</h2>
+    <h2 class="text-3xl font-bold mb-4">Mon Entreprise : Augefi / ECS</h2>
 
-    <div class="grid grid-cols-1 gap-6">
+    <div class="grid grid-cols-1 gap-6 pb-6">
         <!-- Introduction -->
         <div class="bg-white shadow-md rounded-lg p-6">
             <p class="text-gray-700 sm:p-3 text-lg text-justify mb-px">
                 Fort de son équipe de plus de 120 collaborateurs, répartis sur 7 sites géographiques, le cabinet d’audit et expertise comptable AUGEFI accompagne depuis plus de 40 ans ses clients dans tous les secteurs d’activité, y compris agricole. Nous proposons des savoir-faire qui englobent les métiers de la comptabilité, fiscalité, droit du travail, ressources humaines, gestion de patrimoine, marketing et informatique, ainsi que des prestations pour les clients anglophones désireux de s’installer en France.
             </p>
-            <div class="container grid grid-cols-1 sm:grid-cols-2 gap-4 mx-auto p-4">
+            <div class="container grid grid-cols-1 sm:grid-cols-2 gap-4 mx-auto p-4 items-center">
                 <img src="./assets/img/logo/Augefi_logo.png" alt="Photo de Augefi #1"
                     class="w-full h-auto rounded-lg shadow-md hover:scale-105 transition-transform duration-300">
                 <img src="./assets/img/logo/Augefi_bat.jpg" alt="Photo de Augefi #2"
@@ -15,16 +15,22 @@
             </div>
         </div>
 
- 
-            <!-- Section sur ECS et votre rôle -->
-            <div class="bg-slate-50 p-4 rounded-lg shadow-sm">
-                <h3 class="text-black font-extrabold text-lg mb-2">ECS - Support Informatique</h3>
+    </div>
+    
+    <h2 class="text-3xl font-bold mt-6 mb-4">Experts conseils et services - Support Informatique</h2>
 
-                <p class="text-gray-700 mb-4">
-                    Personnellement, j'appartiens à la partie Informatique d'ECS (Experts Conseils et Services), sous la tutelle de Jean-Baptiste Lance. Mon rôle consiste à assurer le bien-être informatique des collaborateurs du groupe Augefi. Je mets en place différentes solutions applicatives afin de simplifier le système d'information.
-                </p>
+    <div class="grid grid-cols-1 gap-6">
+        <!-- Section sur ECS et mon role -->
 
-                <!-- Liste des responsabilités -->
+        <div class="bg-slate-50 p-4 rounded-lg shadow-sm">
+            
+            <p class="text-gray-700 mb-4">
+                Personnellement, j'appartiens à la partie Informatique d'ECS (Experts Conseils et Services), sous la tutelle de Jean-Baptiste Lance. Mon rôle consiste à assurer le bien-être informatique des collaborateurs du groupe Augefi. Je mets en place différentes solutions applicatives afin de simplifier le système d'information.
+            </p>
+            
+            <!-- Liste des responsabilités -->
+
+            <div class="container grid grid-cols-1 sm:grid-cols-2 gap-4 mx-auto p-4">
                 <ul class="list-disc list-inside space-y-2 text-gray-700 pl-4">
                     <li>Support technique aux utilisateurs</li>
                     <li>Déploiement et maintenance des solutions applicatives</li>
@@ -32,7 +38,13 @@
                     <li>Assistance dans l'utilisation des outils numériques</li>
                     <li>Formation des collaborateurs aux nouvelles technologies</li>
                 </ul>
+                <div class="flex items-center justify-center">
+                    <img src="./assets/img/logo/ecs.png" alt="logo ECS"
+                        class="w-100 h-auto rounded-lg shadow-md hover:scale-105 transition-transform duration-300">
+                </div>
             </div>
+
         </div>
     </div>
+
 </section>
